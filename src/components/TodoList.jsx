ff41b9201f5e7d074c4aa2a561b7bd2068cdf840
ahda <ahda@PARHANBOOK>
2024-10-03 17:11:21 +0330

    correected

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,17 +1,13 @@
 import Todolistitem from "./Todolistitem";
 
-export default function TodoList({todos}){
+export default function TodoList({ todos }) {
 
     console.log(todos);
-    return(
+    return (
         <ul className="list-reset">
-
-           {
-                todos.map((t,index)=><Todolistitem key={index} todo={t}/>)
-           }
-               
-             
-            </ul>
-
+            {
+                todos.map((t, index) => <Todolistitem key={index} todo={t} />)
+            }
+        </ul>
     )
 }
--- src/components/Todolistitem.jsx
+++ src/components/Todolistitem.jsx
@@ -1,12 +1,12 @@
 import DelitemtIcon from "./Icons/Delitemicon";
 import EditIcon from "./Icons/EditIcon";
 
-export default function Todolistitem({todos}){
+export default function Todolistitem({todo}){
     return(
         <li className="relative flex items-center justify-between px-2 py-6 border-b">
         <div>
-            <input type="checkbox" checked={todos?.status} onChange={()=>{}} className="" />
-            <p  className={`inline-block mt-1 ml-2 text-gray-600 ${todos?.status ? 'line-through' : ''}`}>{ todos?.title }</p>
+            <input type="checkbox" checked={todo?.status} onChange={()=>{}} className="" />
+            <p  className={`inline-block mt-1 ml-2 text-gray-600 ${todo?.status ? 'line-through' : ''}`}>{ todo?.title }</p>
         </div>
         <button type="button" className="absolute right-0 flex items-center space-x-1">
             <EditIcon />
